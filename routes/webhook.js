cd20f0ebe6e5bf24ac961ff59b6fce8a63fdfd63
Clément Escolano <clement.escolano@icloud.com>
2017-05-30 21:35:54 +0200

    Ajouttt Validation Token

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var config = require('../config/default');
+
 /* GET hello world page. */
 router.get('/webhook', function(req, res, next) {
-  res.render('index');
+  if (req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] === config.validationToken) {
+      console.log("Validating webhook");
+      res.status(200).send(req.query['hub.challenge']);
+    } else {
+      console.error("Failed validation. Make sure the validation tokens match.");
+      res.sendStatus(403);
+    }
 });
 
 module.exports = router;
