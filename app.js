bc78f3ddcf0082e156affc14a3b14c201a19b241
Clément Escolano <clement.escolano@icloud.com>
2017-05-30 21:28:16 +0200

    Correction double webhook

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', homepage);
-app.use('/webhook', webhook);
+app.use('/', webhook);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
